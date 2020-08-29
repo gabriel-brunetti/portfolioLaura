7692bf6dd3a3f5a01ba8b95a6502dfeda6930a6e
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2020-08-28 21:04:06 -0300

    pagina trabalho voluntario concluida

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,7 @@
 // create references to the modal...
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 // to all images -- note I'm using a class!
-var images = document.getElementsByClassName('projeto-img');
+var images = document.getElementsByClassName("projeto-img");
 // the image in the modal
 var modalImg = document.getElementById("img01");
 
@@ -9,37 +9,40 @@ var modalImg = document.getElementById("img01");
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
   // and attach our click listener for this image.
-  img.onclick = function(evt) {
+  img.onclick = function (evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
-    document.querySelector("body").style.overflow = 'hidden';
-  }
+    document.querySelector("body").style.overflow = "hidden";
+  };
 }
 
 var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-  document.querySelector("body").style.overflow = 'visible';
-}
+  document.querySelector("body").style.overflow = "visible";
+};
 
 document.onkeydown = function (e) {
-    if (e.key === 'Escape'){
-        modal.style.display = "none";
-        document.querySelector("body").style.overflow = 'visible';
+  if (e.key === "Escape") {
+    modal.style.display = "none";
+    document.querySelector("body").style.overflow = "visible";
+    if (modalImg.style.maxWidth == "inherit") {
+      modalImg.style.maxWidth = "100vw";
+      modalImg.style.maxHeight = "100vh";
     }
-}
+  }
+};
 
-modalImg.onclick = function() {
-    if(modalImg.style.maxWidth == 'inherit'){
-        modalImg.style.maxWidth = '100vw';
-        modalImg.style.maxHeight = '100vh';
-        span.style.display = 'block';
-        modalImg.style
-    } else {
-        modalImg.style.maxWidth = "inherit";
-        modalImg.style.maxHeight = "inherit";
-        span.style.display = "none";    
-    }
-}
-
+modalImg.onclick = function () {
+  if (modalImg.style.maxWidth == "inherit") {
+    modalImg.style.maxWidth = "100vw";
+    modalImg.style.maxHeight = "100vh";
+    span.style.display = "block";
+    modalImg.style;
+  } else {
+    modalImg.style.maxWidth = "inherit";
+    modalImg.style.maxHeight = "inherit";
+    span.style.display = "none";
+  }
+};
